57791f5f6e7cf8ba00af5c21445b4e623e4d29d6
Maxim Shapovalov <syndex@list.ru>
2024-09-28 23:20:13 +0300

    Initial commit

---
+++ src/test/java/com/github/syndexmx/learningsqlbase_airline_filler_nicroUtil/LearningsqlbaseAirlineFillerMicroUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.learningsqlbase_airline_filler_nicroUtil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningsqlbaseAirlineFillerMicroUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
